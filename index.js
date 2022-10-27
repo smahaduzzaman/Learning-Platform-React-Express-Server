5ad38dcb4f3a216a7361f682b3db99dc80765d82
smahaduzzaman <smahaduzzaman96@gmail.com>
2022-10-27 17:47:33 +0600

    Eight Commit For Updating Data file

--- index.js
+++ index.js
@@ -5,41 +5,36 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-const courses = require('./data/courses.json');
 const categories = require('./data/categories.json');
-const tutorials = require('./data/tutorials.json');
+const courses = require('./data/courses.json');
 
 app.get('/', (req, res) => {
-    res.send('ProTech Edu Server is Running');
+    res.send('PorTech Edu Server is Running');
 })
-app.get('/courses', (req, res) => {
-    res.send(courses);
-})
-
 app.get('/categories', (req, res) => {
     res.send(categories);
 })
 
-app.get('/tutorials', (req, res) => {
-    res.send(tutorials);
+app.get('/courses', (req, res) => {
+    res.send(courses);
+})
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '12') {
+        res.send(courses);
+    } else {
+        const category = news.filter(ct => ct.category_id === id);
+        res.send(category);
+    }
 })
 
 app.get('/courses/:id', (req, res) => {
     const id = req.params.id;
-    if (id === '08') {
-        res.send(courses);
-    } else {
-        const course = courses.filter(ct => ct.courses === id);
-        res.send(course);
-    }
-})
-
-app.get('/tutorials/:id', (req, res) => {
-    const id = req.params.id;
-    const tutorial = tutorials.find(n => n._id == id);
-    res.send(tutorial);
+    const courseItem = courses.find(n => n._id == id);
+    res.send(courseItem);
 })
 
 app.listen(port, () => {
-    console.log(`ProTech Edu Server is Running: ${port}`)
+    console.log(`ProTech Edu Server Running ${port}`)
 })
