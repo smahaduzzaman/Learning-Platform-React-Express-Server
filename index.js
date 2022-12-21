e6cfb9943a3cc812b5e4f3d9b9567ce67f8bba84
smahaduzzaman <smahaduzzaman96@gmail.com>
2022-12-22 03:54:45 +0600

    Tenth Commit For Update Project Polishing

--- index.js
+++ index.js
@@ -15,23 +15,24 @@ app.get('/categories', (req, res) => {
     res.send(categories);
 })
 
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(courses);
+    } else {
+        const category_course = courses.filter(ct => ct.category_id === id);
+        res.send(category_course);
+    }
+
+})
+
 app.get('/courses', (req, res) => {
     res.send(courses);
 })
 
-app.get('/category/:id', (req, res) => {
-    const id = req.params.id;
-    if (id === '12') {
-        res.send(courses);
-    } else {
-        const category = news.filter(ct => ct.category_id === id);
-        res.send(category);
-    }
-})
-
 app.get('/courses/:id', (req, res) => {
     const id = req.params.id;
-    const courseItem = courses.find(n => n._id == id);
+    const courseItem = courses.find(n => n._id === id);
     res.send(courseItem);
 })
 
